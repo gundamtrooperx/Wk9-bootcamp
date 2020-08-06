9b196fb94fb2e393434d18f4743a93120a86184f
Frank Carr <gundamtrooperx@gmail.com>
2020-08-05 20:22:06 -0700

    pushing readme

--- Develop/index.js
+++ Develop/index.js
@@ -53,10 +53,6 @@ const questions = [
 },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
 // function to initialize program
 function init() {
 inquirer.prompt(questions).then(function(tv){
